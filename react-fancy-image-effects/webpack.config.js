4efc40470e924e5eadeabc4fee3f35678c13236e
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-01-20 22:31:25 +0400

    Restore react-fancy-image-effects

---
+++ react-fancy-image-effects/webpack.config.js
@@ -0,0 +1,29 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+  experiments: {
+    syncWebAssembly: true,
+  }
+};
