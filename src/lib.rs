2e7db83e9c0a88f9fe18bbf1a5d4fe089086e670
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-01-21 15:07:35 +0400

    Add application with state

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,37 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn render_canvas(container: &HtmlDivElement) {
+pub fn render_canvas(container: HtmlDivElement) {
     let width = container.client_width();
     let height = container.client_height();
     let message = format!("Container size: {}x{}", width, height);
     container.set_text_content(Some(&message.as_str()));
+}
+
+#[wasm_bindgen]
+struct Application {
+    container: HtmlDivElement,
+    value: f32,
+}
+
+#[wasm_bindgen]
+impl Application {
+    pub fn new(container: HtmlDivElement) -> Self {
+        let value = 0.0 as f32;
+        Self {
+            container,
+            value
+        }
+    }
+
+    pub fn set_value(&mut self, value: f32) {
+        self.value = value;
+    }
+
+    pub fn render(&mut self) {
+        let message = format!("Value: {:.3}", self.value);
+        self.container.set_text_content(
+            Some(&message.as_str())
+        );
+    }
 }
