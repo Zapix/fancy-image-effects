5e83504167adbe2d73d8fdd3018bea5225f244c8
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-06-30 00:14:50 +0200

    Add mui layout

--- react-fancy-image-effects/App.tsx
+++ react-fancy-image-effects/App.tsx
@@ -1,56 +1,22 @@
 import * as React from "react";
-import { ImageShader } from 'fancy-image-effects';
+import { Box, CssBaseline } from '@mui/material';
 
-import image1 from './images/img_5300.jpg';
-import image2 from './images/IMG_5301.jpg';
-import image3 from './images/IMG_5302.jpg';
-import image4 from './images/IMG_5303.jpg';
-import image5 from './images/IMG_5304.jpg';
-import FancyImage from "./src/FancyImage";
+import { Layout } from './src/layout';
+import { AppMenu, FadeColumnExample } from './src/components';
 
-const WIDTH= 512;
-const HEIGHT = 512;
 
 function App() {
-    const [value, setValue] = React.useState(0.0);
-    const [reversed, setReversed] = React.useState(false);
-    const [image, setImage] = React.useState<string>(image3);
-
     return (
-        <>
-            <FancyImage
-                width={WIDTH}
-                height={HEIGHT}
-                shader={ImageShader.ColFade}
-                src={image}
-                value={value}
-                reversed={reversed}
-            />
-            <div>
-                <input
-                    type="range"
-                    min={0}
-                    max={1}
-                    value={value}
-                    onChange={(e) => {setValue(+e.target.value);}}
-                    step="any"
-                />
-            </div>
-            <div>
-                <label><input type="checkbox" value={reversed ? "checked" : ""} onChange={(e) => {
-                    setReversed(e.target.checked);
-                }} />Reversed</label>
-            </div>
-            <div>
-                <select value={image} onChange={(e) => setImage(e.target.value)}>
-                    <option value={image1}>Sherri 1</option>
-                    <option value={image2}>Sherri 2</option>
-                    <option value={image3}>Sherri 3</option>
-                    <option value={image4}>Sherri 4</option>
-                    <option value={image5}>Sherri 5</option>
-                </select>
-            </div>
-        </>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CssBaseline />
+            <Layout
+                renderMenu={AppMenu}
+            >
+                <Box sx={{ grow: 1 }}>
+                    <FadeColumnExample />
+                </Box>
+            </Layout>
+        </Box>
     );
 }
 
---
+++ react-fancy-image-effects/src/components/AppMenu/AppMenu.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {List, ListItem, ListItemText} from '@mui/material';
+
+export const AppMenu = () => (
+    <List>
+        <ListItem>
+            <ListItemText>Column Fade</ListItemText>
+        </ListItem>
+    </List>
+);
